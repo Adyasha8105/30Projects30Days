84fc6537d4020b9d0cf3a0b16f81a5f8c6556e35
adyasha <adyashamohanty8105@gmail.com>
2020-12-31 01:02:55 +0530

    added the expanding cards

--- Day1/script.js
+++ Day1/script.js
@@ -1,1 +1,14 @@
-//Javascript
+const panels = document.querySelectorAll('.panel') //select all the elements have this class
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
